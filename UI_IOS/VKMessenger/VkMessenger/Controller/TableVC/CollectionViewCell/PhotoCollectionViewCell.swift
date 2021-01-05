935a84797290559e1d27b68ba35143b955f0b4c2
Artyom <art.ustinov@mail.ru>
2021-01-05 10:31:24 +0300

    Fix shadowcolor

--- UI_IOS/VKMessenger/VkMessenger/Controller/TableVC/CollectionViewCell/PhotoCollectionViewCell.swift
+++ UI_IOS/VKMessenger/VkMessenger/Controller/TableVC/CollectionViewCell/PhotoCollectionViewCell.swift
@@ -13,11 +13,11 @@ class PhotoCollectionViewCell: UICollectionViewCell {
         }
         self.count += 1
         self.countOfLike.text = "\(self.count)"
-//        imagePhoto.layer.shadowColor = UIColor.black.cgColor
-//        imagePhoto.layer.shadowOffset = CGSize(width: -10, height: 10)
-//        imagePhoto.layer.shadowRadius = 20
-//        imagePhoto.layer.shadowOpacity = 0.5
-        imagePhoto.layer.backgroundColor = UIColor.red.cgColor
+        imagePhoto.clipsToBounds = false
+        imagePhoto.layer.shadowColor = UIColor.red.cgColor
+        imagePhoto.layer.shadowOpacity = 1
+        imagePhoto.layer.shadowOffset = CGSize.zero
+        imagePhoto.layer.shadowRadius = 10
     }
     
     @IBAction func dislikeButton(_ sender: UIButton) {
@@ -26,6 +26,10 @@ class PhotoCollectionViewCell: UICollectionViewCell {
         }
         self.count -= 1
         self.countOfLike.text = "\(self.count)"
-        imagePhoto.layer.backgroundColor = UIColor.black.cgColor
+        imagePhoto.clipsToBounds = false
+        imagePhoto.layer.shadowColor = UIColor.white.cgColor
+        imagePhoto.layer.shadowOpacity = 1
+        imagePhoto.layer.shadowOffset = CGSize.zero
+        imagePhoto.layer.shadowRadius = 30
     }
 }
