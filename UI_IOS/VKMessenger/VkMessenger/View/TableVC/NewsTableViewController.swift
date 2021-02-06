124f67b9a3e7f57df63555a80360f31a75f4c06d
Artyom <art.ustinov@mail.ru>
2021-02-06 14:39:52 +0300

    update

--- UI_IOS/VKMessenger/VkMessenger/View/TableVC/NewsTableViewController.swift
+++ UI_IOS/VKMessenger/VkMessenger/View/TableVC/NewsTableViewController.swift
@@ -21,7 +21,7 @@ class NewsTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: NewsTableViewCell.identifer, for: indexPath) as? NewsTableViewCell{
             cell.labelNews.text = "NEWS with XIB cell"
-            cell.imageNews?.image = UIImage.init(named: "3")
+            cell.imageNews?.image = UIImage.init(named: "9")
             return cell
         }
         return UITableViewCell()
