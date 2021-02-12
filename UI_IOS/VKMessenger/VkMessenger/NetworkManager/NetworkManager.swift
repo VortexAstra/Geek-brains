6042fe2ba7054b705a83f323ffa58f1620f9d67d
Artyom <art.ustinov@mail.ru>
2021-02-12 14:29:47 +0300

    update

--- UI_IOS/VKMessenger/VkMessenger/NetworkManager/NetworkManager.swift
+++ UI_IOS/VKMessenger/VkMessenger/NetworkManager/NetworkManager.swift
@@ -1,11 +1,44 @@
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class NetworkManager {
     
-    static let session = NetworkManager()
+    static let shared = NetworkManager()
+    
+    var token: String?
+    var userId: Int?
     
     private init() {}
     
-    private var token: String?
-    private var userId: Int?
+    private static let sessionAF: Alamofire.Session = {
+        let configuration = URLSessionConfiguration.default
+        configuration.allowsCellularAccess = false
+        let session = Alamofire.Session(configuration: configuration)
+        
+        return session
+    }()
+    
+    static func loadInfo(for item:String, token: String) {
+        print("------------------------\(item)--------------------------------------------")
+        let baseURL = "https://api.vk.com"
+        let path = "/method/\(item).get"
+        
+        let params: Parameters = [
+            "access_token": token,
+            "fields": "first_name, photo_50",
+            "extended": 1,
+            "v": "5.92"
+        ]
+        
+        NetworkManager.sessionAF.request(baseURL + path, method: .get, parameters: params).responseJSON { (response) in
+            switch response.result{
+            case .success(let value):
+                let json = JSON(value)
+                print(json["response"]["items"])
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 }
---
+++ UI_IOS/VKMessenger/VkMessenger/NetworkManager/VKLoginViewController.swift
@@ -0,0 +1,75 @@
+import UIKit
+import WebKit
+import Alamofire
+import SwiftyJSON
+
+class VKLoginViewController: UIViewController {
+
+    @IBOutlet weak var webView: WKWebView! {
+        didSet {
+            webView.navigationDelegate = self
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "oauth.vk.com"
+        components.path = "/authorize"
+        components.queryItems = [
+            URLQueryItem(name: "client_id", value: "7758085"),
+            URLQueryItem(name: "display", value: "mobile"),
+            URLQueryItem(name: "redirect_uri", value: "https://oauth.vk.com/blank.html"),
+            URLQueryItem(name: "scope", value: "262154"),
+            URLQueryItem(name: "response_type", value: "token"),
+            URLQueryItem(name: "v", value: "5.92")
+        ]
+
+        guard let url = components.url else {return}
+        let request = URLRequest(url: url)
+        webView.load(request)
+
+    }
+    
+
+}
+
+extension VKLoginViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+        guard let url = navigationResponse.response.url,
+              url.path == "/blank.html",
+              let fragment = url.fragment else {
+            decisionHandler(.allow)
+            return
+        }
+        
+        let params = fragment
+            .components(separatedBy: "&")
+            .map { $0.components(separatedBy: "=") }
+            .reduce([String: String]()) { result, param in
+                var dict = result
+                let key = param[0]
+                let value = param[1]
+                dict[key] = value
+                
+                return dict
+            }
+        
+        print(params)
+        
+        guard let token = params["access_token"],
+              let userIdString = params["user_id"],
+              let _ = Int(userIdString) else {
+            decisionHandler(.allow)
+            return
+        }
+        
+        NetworkManager.shared.token = token
+        
+        NetworkManager.loadInfo(for: "friends", token: token)
+        NetworkManager.loadInfo(for: "groups", token: token)
+        decisionHandler(.cancel)
+    }
+}
