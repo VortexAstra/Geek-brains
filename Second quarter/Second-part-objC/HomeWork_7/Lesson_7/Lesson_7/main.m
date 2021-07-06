4d6d35d95c8a90473542355ccdb3ff101c121f24
Ustinov Artyom <artem.ustinov@corp.mail.ru>
2021-07-06 16:42:21 +0300

    add hw6

---
+++ Second quarter/Second-part-objC/HomeWork_7/Lesson_7/Lesson_7/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Lesson_7
+//
+//  Created by artem.ustinov on 06.07.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
