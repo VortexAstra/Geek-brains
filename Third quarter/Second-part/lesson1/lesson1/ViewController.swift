daf7dc4487b41898db96fa7873fc8a82fa9d73cd
artem.ustinov <artem.ustinov@corp.mail.ru>
2021-10-09 15:44:55 +0300

    Init commit

---
+++ Third quarter/Second-part/lesson1/lesson1/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  lesson1
+//
+//  Created by artem.ustinov on 08.10.2021.
+//
+
+import UIKit
+import GoogleMaps
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mapView: GMSMapView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
