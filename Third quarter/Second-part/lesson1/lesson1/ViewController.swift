454cc387d9f1b1583f39f2f0606607cac5668e1e
artem.ustinov <artem.ustinov@corp.mail.ru>
2021-10-10 01:01:10 +0300

    Lesson_1

--- Third quarter/Second-part/lesson1/lesson1/ViewController.swift
+++ Third quarter/Second-part/lesson1/lesson1/ViewController.swift
@@ -7,16 +7,80 @@
 
 import UIKit
 import GoogleMaps
+import CoreLocation
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
-    @IBOutlet weak var mapView: GMSMapView!
+//  MARK: - IB
+    @IBOutlet private weak var mapView: GMSMapView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction private func isTrackingButton(_ button: UIBarButtonItem) {
+        locationManager.startUpdatingLocation()
     }
 
+//  MARK: - PROPERTY
+    private let locationManager = CLLocationManager()
+    private lazy var coordinate = CLLocationCoordinate2D(latitude: 55.753215, longitude: 37.622504)
+    private lazy var camera = GMSCameraPosition.camera(withTarget: coordinate, zoom: 17)
 
+    private var isTracking = false
+
+//  MARK: - LIFECYCLE
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupMapView()
+        setupLocationManager()
+    }
 }
 
+private extension ViewController {
+
+    func setupMapView() {
+        mapView.camera = camera
+        mapView.settings.compassButton = true
+        mapView.settings.myLocationButton = true
+        mapView.isTrafficEnabled = true
+        mapView.delegate = self
+        applyCustomStyle()
+    }
+
+    func setupLocationManager() {
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
+    }
+
+    func addMarker(to coordination: CLLocationCoordinate2D) {
+        let marker = GMSMarker(position: coordination)
+        marker.icon = GMSMarker.markerImage(with: nil)
+        marker.title = "Custom title"
+        marker.map = mapView
+    }
+
+    func applyCustomStyle() {
+        guard
+            let url = Bundle.main.url(forResource: "map-style", withExtension: "json")
+        else { return }
+
+        mapView.mapStyle = try? GMSMapStyle(contentsOfFileURL: url)
+    }
+}
+
+
+//  MARK: - GMSMapViewDelegate
+extension ViewController: GMSMapViewDelegate {
+
+    func didTapMyLocationButton(for mapView: GMSMapView) -> Bool {
+        return true
+    }
+}
+
+//  MARK: - CLLocationManagerDelegate
+extension ViewController: CLLocationManagerDelegate {
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let lastLocation = locations.last else { return }
+        mapView.animate(to: GMSCameraPosition.camera(withTarget: lastLocation.coordinate, zoom: 17))
+        addMarker(to: lastLocation.coordinate)
+    }
+}
